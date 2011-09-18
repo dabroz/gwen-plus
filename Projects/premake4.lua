3531470f638a023160d895c4859dc2a238b75198
garrynewman@gmail.com <garrynewman@gmail.com@82b74e7c-beea-4a93-d681-cf472943600a>
2011-09-18 18:26:32 +0000

    Linux compile fixes

--- Projects/premake4.lua
+++ Projects/premake4.lua
@@ -53,7 +53,7 @@ project "GWEN Static"
 		
 project "UnitTest"
 	uuid ( "00F7E527-468D-4b79-9A35-F3386C0CF7F9" )
-	files { "../unittest/**.*" }
+	files { "../UnitTest/**.*" }
 	flags { "Symbols" }
 	kind "StaticLib"
 	
@@ -62,18 +62,6 @@ project "UnitTest"
 		
 	configuration "Debug"
 		targetname( "unittestd" )
-	
-project "Renderer-DirectX9"
-	uuid ( "2B44E691-45AD-4d4c-80B2-14028BB2DD36" )
-	files { "../Renderers/DirectX9/DirectX9.cpp" }
-	flags { "Symbols" }
-	kind "StaticLib"
-	
-	configuration "Release"
-		targetname( "GWEN-Renderer-DirectX9" )
-		
-	configuration "Debug"
-		targetname( "GWEN-Renderer-DirectX9d" )
 		
 project "Renderer-OpenGL"
 	uuid ( "C105C99D-90DA-4a81-8BFA-A35AD91F1A91" )
@@ -100,18 +88,7 @@ project "Renderer-OpenGL_DebugFont"
 	configuration "Debug"
 		targetname( "GWEN-Renderer-OpenGL_DebugFont" )
 
-project "Renderer-GDI"
-	uuid ( "617E3FC9-9449-4599-8A48-899989C171B2" )
-	files { "../Renderers/GDIPlus/GDIPlus.cpp" }
-	files { "../Renderers/GDIPlus/GDIPlusBuffered.cpp" }
-	flags { "Symbols" }
-	kind "StaticLib"
-	
-	configuration "Release"
-		targetname( "GWEN-Renderer-GDI" )
-		
-	configuration "Debug"
-		targetname( "GWEN-Renderer-GDI" )
+
 		
 project "Renderer-SFML"
 	uuid ( "C462DEDF-B4FD-4cc9-9288-337B9FD0E4D2" )
@@ -125,20 +102,7 @@ project "Renderer-SFML"
 	configuration "Debug"
 		targetname( "GWEN-Renderer-SFML" )
 		
-project "Sample-DirectX9"
-	uuid ( "033DFF57-E9FD-480b-9CD4-88A41E51923C" )
-	targetdir ( "../bin" )
-	debugdir ( "../bin" )
-	files { "../Samples/Direct3D/Direct3DSample.cpp" }
-	kind "WindowedApp"
-	
-	links { "Renderer-DirectX9", "GWEN Static", "UnitTest" }
 
-	configuration "Release"
-		targetname( "DX9Sample" )
-		
-	configuration "Debug"
-		targetname( "DX9Sample_D" )
 		
 project "Sample-OpenGL"
 	uuid ( "797F4AE2-8804-4a25-B61F-D53056D6B93C" )
@@ -171,19 +135,7 @@ project "Sample-OpenGL_DebugFont"
 	configuration "Debug"
 		targetname( "OpenGLDebugFontSample_D" )
 
-project "Sample-WindowsGDI"
-	uuid ( "3C9B0F29-1EE5-4653-93F0-5BE242190512" )
-	targetdir ( "../bin" )
-	debugdir ( "../bin" )
-	files { "../Samples/WindowsGDI/WindowsGDI.cpp" }
-	kind "WindowedApp"
-	links { "Renderer-GDI", "GWEN Static", "UnitTest" }	
-	
-	configuration "Release"
-		targetname( "GDISample" )
-		
-	configuration "Debug"
-		targetname( "GDISample_D" )
+
 		
 project "Sample-SFML"
 	uuid ( "955FF76A-C25F-4bac-835C-4F958252F1FC" )
@@ -191,7 +143,7 @@ project "Sample-SFML"
 	debugdir ( "../bin" )
 	files { "../Samples/SFML/SFML.cpp" }
 	kind "WindowedApp"
-	links { "Renderer-SFML", "GWEN Static", "UnitTest" }
+	links { "Renderer-SFML", "UnitTest", "GWEN Static" }
 	
 	configuration "Release"
 		targetname( "SFMLSample" )
@@ -200,3 +152,60 @@ project "Sample-SFML"
 	configuration "Debug"
 		targetname( "SFMLSample_D" )
 		links { "sfml-main-d", "sfml-window-s-d", "sfml-graphics-s-d", "sfml-system-s-d", "opengl32" }
+		
+if ( os.get() == "windows" ) then
+
+	project "Sample-WindowsGDI"
+		uuid ( "3C9B0F29-1EE5-4653-93F0-5BE242190512" )
+		targetdir ( "../bin" )
+		debugdir ( "../bin" )
+		files { "../Samples/WindowsGDI/WindowsGDI.cpp" }
+		kind "WindowedApp"
+		links { "Renderer-GDI", "GWEN Static", "UnitTest" }	
+		
+		configuration "Release"
+			targetname( "GDISample" )
+			
+		configuration "Debug"
+			targetname( "GDISample_D" )
+			
+	project "Sample-DirectX9"
+		uuid ( "033DFF57-E9FD-480b-9CD4-88A41E51923C" )
+		targetdir ( "../bin" )
+		debugdir ( "../bin" )
+		files { "../Samples/Direct3D/Direct3DSample.cpp" }
+		kind "WindowedApp"
+		
+		links { "Renderer-DirectX9", "GWEN Static", "UnitTest" }
+
+		configuration "Release"
+			targetname( "DX9Sample" )
+			
+		configuration "Debug"
+			targetname( "DX9Sample_D" )
+			
+	project "Renderer-GDI"
+		uuid ( "617E3FC9-9449-4599-8A48-899989C171B2" )
+		files { "../Renderers/GDIPlus/GDIPlus.cpp" }
+		files { "../Renderers/GDIPlus/GDIPlusBuffered.cpp" }
+		flags { "Symbols" }
+		kind "StaticLib"
+		
+		configuration "Release"
+			targetname( "GWEN-Renderer-GDI" )
+			
+		configuration "Debug"
+			targetname( "GWEN-Renderer-GDI" )
+			
+	project "Renderer-DirectX9"
+		uuid ( "2B44E691-45AD-4d4c-80B2-14028BB2DD36" )
+		files { "../Renderers/DirectX9/DirectX9.cpp" }
+		flags { "Symbols" }
+		kind "StaticLib"
+		
+		configuration "Release"
+			targetname( "GWEN-Renderer-DirectX9" )
+			
+		configuration "Debug"
+			targetname( "GWEN-Renderer-DirectX9d" )
+end
